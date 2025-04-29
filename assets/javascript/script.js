d57e6dc6df6aff7e12b18e737cf41283e6281ca1
Ryan Ford <fordryan01@hotmail.co.uk>
2025-04-29 11:59:15 +0100

    added the code for the subtraction and multiplication questions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -23,7 +23,15 @@ function runGame(gameType) {
 
     if (gameType === "addition"){
         displayAdditionQuestion(num1, num2);
-    } else{
+    } else if (gameType === "multiply"){
+        displayMultiplyQuestion(num1,num2);
+    } else if (gameType === "subtract"){
+        if (num1 > num2){
+            displaySubtractQuestion(num1, num2);
+        } else {
+            displaySubtractQuestion(num2, num1);
+        }
+    }else {
         alert(`Unknown game type ${gameType}`)
         throw `Unknown game type: ${gameType}. Aborting`;
     }
@@ -50,7 +58,15 @@ function calculateCorrectAnswer(){
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    } else{
+    } else if (operator === "x"){
+        return [operand1 * operand2, "multiply"];
+    } else if (operator === "-"){
+        if (operand1 > operand2){
+            return [operand1 - operand2, "subtract"];
+        } else{
+            return [operand2 - operand1, "subtract"];
+        }
+    }else{
         alert(`Unmimplemented Operator ${operator}`);
         throw `unimplemented operator ${operator}. Aborting`;
     }
@@ -74,12 +90,16 @@ function displayAdditionQuestion(operand1, operand2){
     document.getElementById("operator").textContent = "+";
 }
 
-function displaySubtractQuestion(){
-
+function displaySubtractQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "-";
 }
 
-function displayMultiplyQuestion(){
-
+function displayMultiplyQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 }
 
 function displayDivisionQuestion(){
